f51d74eb1d166550078d1a838fd56722338d3904
zychp <pawelzych97@gmail.com>
2018-11-12 16:20:01 +0100

    tests cleanup

--- test/crud/UpdateDAOTest.java
+++ test/crud/UpdateDAOTest.java
@@ -6,12 +6,9 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import sun.security.x509.IPAddressName;
 
 class CRUDTest {
     private SessionFactory sessionFactory;
@@ -34,19 +31,16 @@ class CRUDTest {
     void updateBike() {
         final double UPDATED_PRICE = 420;
         Transaction transaction = session.beginTransaction();
-
         Bike bike = new Bike("testowaNazwa", "testowaMarka", "testowyRodzaj", 120);
         session.save(bike);
         transaction.commit();
         int bikeId = bike.getId();
         System.out.println("BikeId: " + bikeId);
-
         bike.setPrice(UPDATED_PRICE);
 
         UpdateDAO instance = new UpdateDAO();
         instance.update(bikeId, bike);
         
-       
         Transaction transaction1 = session.beginTransaction();
         Bike updatedBike = (Bike) session.get(Bike.class, bikeId);
         transaction1.commit();
